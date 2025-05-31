7a1c0d75ab461e61bc66915c3070d1feef984f97
Vedik Saini <vediksaini0704@gmail.com>
2025-05-31 17:34:15 +0530

    write triple contract logic for practice

---
+++ triple-contract/src/lib.rs
@@ -0,0 +1,34 @@
+use borsh::{BorshSerialize, BorshDeserialize};
+use solana_program::{account_info::{next_account_info, AccountInfo},
+ entrypoint::{ ProgramResult},
+ entrypoint,
+ pubkey:: Pubkey
+};
+
+entrypoint!(process_instruction);
+
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+struct TripleCounter{
+    count: u32
+}
+
+fn process_instruction(
+    program_id : &Pubkey,
+    accounts : &[AccountInfo],
+    instruction_data : &[u8]
+)-> ProgramResult{
+    let mut iter = accounts.iter();
+    let data_account = next_account_info(&mut iter)?;
+
+    let mut counter = TripleCounter::try_from_slice(& data_account.data.borrow())?;
+
+    if counter.count == 0{
+        counter.count = 1;
+    }else {
+        counter.count = counter.count * 3;
+    };
+
+    counter.serialize(&mut *data_account.data.borrow_mut())?;
+
+    Ok(())
+}
